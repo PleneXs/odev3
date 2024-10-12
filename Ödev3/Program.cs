c757a919623dccae4072e682692a5b76b2eef9f1
Melik Ali Turan <melikalituran26@gmail.com>
2024-10-12 10:46:33 +0300

    Proje dosyası ekle.

---
+++ Ödev3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+
+    static void Main()
+    {
+        // Kullanıcıdan alınması gereken bilgi
+        Console.Write("Birinci kenarın uzunluğunu girin: ");
+        int kenar1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("İkinci kenarın uzunluğnu girin: ");
+        int kenar2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Üçüncü kenarın uzunluğunu girin: ");
+        int kenar3 = Convert.ToInt32(Console.ReadLine());
+
+        // Çevreyi hesapla
+        int cevre = kenar1 + kenar2 + kenar3;
+
+        // Sonucu ekrana yazdır
+        Console.WriteLine($"Üçgenin Çevresi: {cevre}");
+
+    }
+}
+
+
